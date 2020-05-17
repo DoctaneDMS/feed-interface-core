181aa8a893e90df51013fd5fced3dac1489c06ff
Jonathan Essex <jonathan.essex@softwareplumbers.com>
2020-05-17 08:12:35 -0400

    Fleshed out standard exception classes

--- src/main/java/com/softwareplumbers/feed/FeedExceptions.java
+++ src/main/java/com/softwareplumbers/feed/FeedExceptions.java
@@ -5,8 +5,13 @@
  */
 package com.softwareplumbers.feed;
 
+import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 
 /** Exception classes for Feed services.
  *
@@ -14,14 +19,34 @@ import java.util.function.Consumer;
  */
 public class FeedExceptions {
     
+    public static enum Type {
+        INVALID_PATH,
+        INVALID_JSON
+    }
+    
     /** Base exception.
      * 
      * All checked Feed exceptions will be a subclass.
      * 
      */
     public static class BaseException extends Exception {
-        public BaseException(String reason) {
+        public final Type type;
+        public BaseException(Type type, String reason) {
             super(reason);
+            this.type = type;
+        }
+        public static Optional<Type> getType(JsonObject obj) {
+            try {
+                return Optional.of(Type.valueOf(obj.getString("type")));
+            } catch (Exception e) {
+                return Optional.empty();
+            }
+        }
+        public JsonObjectBuilder buildJson(JsonObjectBuilder bldr) {
+            return bldr.add("type", type.toString());
+        }
+        public JsonObject toJson() {
+            return buildJson(Json.createObjectBuilder()).build();
         }
     }
     
@@ -31,14 +56,58 @@ public class FeedExceptions {
         }
     }
     
+    
+    /** Throw if a path is invalid
+     * 
+     */
+    public static class InvalidJson extends BaseException {
+        public final JsonObject json;
+        public InvalidJson(JsonObject json) {
+            super(Type.INVALID_JSON, "Invalid Json: " + json.toString());
+            this.json = json;
+        }
+        public static Optional<JsonObject> getJson(JsonObject object) {
+            try {
+                return Optional.of(object.getJsonObject("json"));
+            } catch (Exception e) {
+                return Optional.empty();
+            }
+        }
+        public JsonObjectBuilder buildJson(JsonObjectBuilder bldr) {
+            return super.buildJson(bldr).add("json", json);
+        }
+    }
+
     /** Throw if a path is invalid
      * 
      */
     public static class InvalidPath extends BaseException {
         public final FeedPath path;
         public InvalidPath(FeedPath path) {
-            super("Invalid Path: " + path.toString());
+            super(Type.INVALID_PATH, "Invalid Path: " + path.toString());
             this.path = path;
+        }
+        public static Optional<FeedPath> getPath(JsonObject object) {
+            try {
+                return Optional.of(FeedPath.valueOf(object.getString("path")));
+            } catch (Exception e) {
+                return Optional.empty();
+            }
+        }
+        public JsonObjectBuilder buildJson(JsonObjectBuilder bldr) {
+            return super.buildJson(bldr).add("path", path.toString());
+        }
+    }
+    
+    public static BaseException build(JsonObject json) throws InvalidJson {
+        Type type = BaseException.getType(json).orElseThrow(()->new InvalidJson(json));
+        switch(type) {
+            case INVALID_PATH:
+                return new InvalidPath(InvalidPath.getPath(json).orElseThrow(()->new InvalidJson(json)));
+            case INVALID_JSON:
+                return new InvalidJson(InvalidJson.getJson(json).orElseThrow(()->new InvalidJson(json)));
+            default:
+                throw new InvalidJson(json);
         }
     }
     
